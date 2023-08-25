ffb0324473d05d9bc7531baea69ad3fd40620ee2
eunsookim1 <92eunsoo@gmail.com>
2023-08-24 22:21:27 -0700

    changing object names within class

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the first store (with id = 1)
+
+@store1 = Store.first
+@store2 = Store.second
+@store1.update(name: 'White Rock')
+
+puts "Updated Store 1 Name: #{@store1.name}"
