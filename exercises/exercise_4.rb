08cbad05f475d6af088031aea8eb5f1a75ede7b9
eunsookim1 <92eunsoo@gmail.com>
2023-08-25 20:42:51 -0700

    delete id

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,9 +7,9 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-Store.create(id: 4, name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-Store.create(id: 5, name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
-Store.create(id: 6, name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 # Fetch stores that carry men's apparel and assign them to @mens_stores
 @mens_stores = Store.where(mens_apparel: true)
